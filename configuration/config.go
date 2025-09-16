24f2610695b7338380a00558b2165f94bb02f9a5
Tsung-Yuan,Hsieh <dennesshen@gmail.com>
2025-09-16 13:32:05 +0800

    redo the process of read env par

--- configuration/config.go
+++ configuration/config.go
@@ -2,14 +2,19 @@ package configuration
 
 import (
 	"context"
-	"github.com/spf13/viper"
+	"fmt"
 	"log/slog"
+	"os"
 	"reflect"
+	"strings"
+
+	"github.com/spf13/viper"
 )
 
 var (
 	configs   = make([]interface{}, 0)
 	pathArray = []string{"./src/resources", "./src", "."}
+	postfixes = []string{".yaml", ".yml"}
 )
 
 func Register(config ...interface{}) {
@@ -23,20 +28,50 @@ func Register(config ...interface{}) {
 
 func InitConfiguration() {
 
-	viper.AutomaticEnv()
+	//把所有環境變數塞進 viper
+	for _, e := range os.Environ() {
+		parts := strings.SplitN(e, "=", 2)
+		if len(parts) == 2 {
+			key := strings.ToLower(parts[0]) // 轉小寫，避免 key 太醜
+			viper.Set(key, parts[1])
+		}
+	}
 
 	for _, path := range pathArray {
-		viper.SetConfigFile(path + "/application.yaml")
-		viper.SetConfigType("yaml")
-		_ = viper.MergeInConfig()
+		for _, postfix := range postfixes {
+			viper.SetConfigFile(path + "/config" + postfix)
+			viper.SetConfigType("yaml")
+			_ = viper.MergeInConfig()
+		}
+	}
+
+	for _, path := range pathArray {
+		for _, postfix := range postfixes {
+			viper.SetConfigFile(path + "/application" + postfix)
+			viper.SetConfigType("yaml")
+			_ = viper.MergeInConfig()
+		}
+	}
+
+	for _, key := range viper.AllKeys() {
+		fmt.Printf("%s = %v\n", key, viper.Get(key))
 	}
 
 	env, ok := viper.Get("env.name").(string)
 	if ok {
 		for _, path := range pathArray {
-			viper.SetConfigFile(path + "/application-" + env + ".yaml")
-			viper.SetConfigType("yaml")
-			_ = viper.MergeInConfig()
+			for _, postfix := range postfixes {
+				viper.SetConfigFile(path + "/config-" + env + postfix)
+				viper.SetConfigType("yaml")
+				_ = viper.MergeInConfig()
+			}
+		}
+		for _, path := range pathArray {
+			for _, postfix := range postfixes {
+				viper.SetConfigFile(path + "/application-" + env + postfix)
+				viper.SetConfigType("yaml")
+				_ = viper.MergeInConfig()
+			}
 		}
 	}
 
